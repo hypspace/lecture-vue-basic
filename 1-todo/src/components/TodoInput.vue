2008e5e8f761699d708be0e549909fd949d304fa
Tae-hyeon Kim <hypspacecraft@gmail.com>
2024-09-30 12:22:42 +0900

    [1-todo] chore: implement todo saving functionality in TodoInput component

--- 1-todo/src/components/TodoInput.vue
+++ 1-todo/src/components/TodoInput.vue
@@ -1,13 +1,33 @@
 <template>
   <div>
-    <input placeholder="Add a new todo" autofocus />
-    <button type="button">add</button>
+    <input
+      placeholder="Add a new todo"
+      v-model="newTodo"
+      v-on:keyup.enter="addTodo"
+      autofocus
+    />
+    <button type="button" v-on:click="addTodo">add</button>
   </div>
 </template>
 
 <script>
 export default {
   name: 'TodoInput',
+  data: function () {
+    return {
+      newTodo: '',
+    }
+  },
+  methods: {
+    addTodo: function () {
+      console.log('[TodoInput]', 'addTodo()', this.newTodo)
+      localStorage.setItem(this.newTodo, this.newTodo)
+      this.clearInput()
+    },
+    clearInput: function () {
+      this.newTodo = ''
+    },
+  },
 }
 </script>
 
